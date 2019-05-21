76764b06990c09b2377f318cf14289688217e318
M. Waldburger <waldburger91@hotmail.com>
2019-05-21 07:20:35 +0200

    add repository / data access layer

---
+++ MonitoringClient/Repository/IRepositoryBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringClient.Repository
+{
+    public interface IRepositoryBase<M>
+    {
+        string ConnectionString { get;}
+        /// <summary>
+        /// Zählt in der Datenbank die Anzahl Model-Objekte vom Typ M, die der
+        /// Where-Bedingung entsprechen
+        /// </summary>
+        /// <param name="whereCondition">WhereBedingung als string
+        /// z.B. "NetPrice > @netPrice and Active = @active and
+        /// Description like @desc</param>
+        /// <param name="parameterValues">Parameter-Werte für die Wherebedingung
+        /// bspw: {{"netPrice", 10.5}, {"active", true}, {"desc", "Wolle%"}}</param>
+        /// <returns></returns>
+        long Count(string whereCondition, Dictionary<string, object> parameterValues);
+        /// <summary>
+        /// Zählt alle Model-Objekte vom Typ M
+        /// </summary>
+        /// <returns></returns>
+        long Count();
+        /// <summary>
+        /// Gibt den Tabellennamen zurück, auf die sich das Repository bezieht
+        /// </summary>
+        string TableName { get; }
+    }
+}
---
+++ MonitoringClient/Repository/RepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringClient.Repository
+{
+    public abstract class RepositoryBase<M> : IRepositoryBase<M>
+    {
+        public string TableName { get; }
+        public string ConnectionString { get; }
+
+        public RepositoryBase()
+        {
+            this.ConnectionString = "Server = localhost; Database = ; Uid = root; Pwd = ;";
+        }
+
+        public long Count(string whereCondition, Dictionary<string, object> parameterValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Count()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
