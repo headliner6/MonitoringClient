5e30b80f731fdb671915cf1b38a5c8c5b4a9f366
M. Waldburger <waldburger91@hotmail.com>
2019-06-05 08:52:49 +0200

    add tree structure

---
+++ MonitoringClient/DataStructures/LocationTree.cs
@@ -0,0 +1,28 @@
+﻿using MonitoringClient.Model;
+using System;
+using System.Collections.Generic;
+
+namespace MonitoringClient.DataStructures
+{
+    public class LocationTree
+    {
+        public Node<LocationsModel> Parent { get; set; }
+        private readonly List<Node<LocationsModel>> _childrenNodes;
+        public IReadOnlyList<Node<LocationsModel>> ChildrenNodes
+        {
+            get { return _childrenNodes; }
+        }
+        public LocationTree()
+        {
+            Parent = null;
+        }
+
+        public void Insert(Node<LocationsModel> location)
+        {
+            if (Parent.Element.Id == location.Element.Id)
+            {
+                //Parent.
+            }
+        }
+    }
+}
---
+++ MonitoringClient/DataStructures/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringClient.DataStructures
+{
+    public class Node<T>
+    {
+        public T Element { get; set; }
+    }
+}
