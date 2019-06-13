f89a1aec2004d09f42dd00723cd5e7d477133aa4
Waldi6 <waldburger91@hotmail.com>
2019-06-13 23:45:00 +0200

    refactoring

--- MonitoringClient/MainWindow.xaml.cs
+++ MonitoringClient/MainWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace MonitoringClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
