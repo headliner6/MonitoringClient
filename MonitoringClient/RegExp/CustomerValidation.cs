6257e0dc323cc9133a51da9bd0b6d21efaf1c9d0
Waldi6 <waldburger91@hotmail.com>
2019-06-21 21:37:39 +0200

    add customervalidation

---
+++ MonitoringClient/RegExp/CustomerValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringClient.RegExp
+{
+    public class CustomerValidation
+    {
+        public void PersonIdValidation() { }
+        public void AdressnumberValidation() { }
+        public void CustomerAccountNumberValidation() { }
+        public void PhoneNumberValidation() { }
+        public void EmailValidation() { }
+        public void WebsiteValidation() { }
+        public void PasswordValidation() { }
+
+
+    }
+}
