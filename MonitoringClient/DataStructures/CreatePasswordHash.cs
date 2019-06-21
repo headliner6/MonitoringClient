edfa595a9b04a5595cffe78f74a1f36d9f6386e0
Waldi6 <waldburger91@hotmail.com>
2019-06-21 21:25:43 +0200

    add customermodel, createpasswordhash

---
+++ MonitoringClient/DataStructures/CreatePasswordHash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringClient.DataStructures
+{
+    public class CreatePasswordHash
+    {
+        public string GetMD5Hash(string input)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5Provider = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bytes = Encoding.Unicode.GetBytes(input);
+            bytes = md5Provider.ComputeHash(bytes);
+            StringBuilder s = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+    }
+}
---
+++ MonitoringClient/Model/CustomerModel.cs
@@ -0,0 +1,28 @@
+﻿using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringClient.Model
+{
+    [Table ("customer")]
+    public class CustomerModel
+    {
+        [Column ("PERSON_FK")]
+        public int PersonId { get; set; }
+        [Column("ADRESS_FK")]
+        public int Adressnumber { get; set; }
+        [Column("KUNDENKONTO_FK")]
+        public int CustomerAccountNumber { get; set; }
+        [Column("TEL")]
+        public string PhoneNumber { get; set; }
+        [Column("EMAIL")]
+        public string Email { get; set; }
+        [Column("URL")]
+        public string Website { get; set; }
+        [Column("PASSWORD")]
+        public string Password { get; set; }
+    }
+}
