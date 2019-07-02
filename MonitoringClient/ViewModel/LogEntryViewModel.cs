41b835ea43674c2ccd64af80e68b628c3fa42a11
Waldi6 <waldburger91@hotmail.com>
2019-07-02 21:11:42 +0200

    refactoring

--- MonitoringClient/ViewModel/LogEntryViewModel.cs
+++ MonitoringClient/ViewModel/LogEntryViewModel.cs
@@ -62,6 +62,10 @@ namespace MonitoringClient.ViewModel
         }
         public void GetAll()
         {
+            int? x;
+            x = null;
+            int x1 = x.HasValue ? x.Value : default(int);
+
             try
             {
                 _logEntryModelRepository.ConnectionString = ConnectionString;
