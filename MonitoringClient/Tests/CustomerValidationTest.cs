c7f99b6b8076a146dba3994a8710165b23189d0b
Waldi6 <waldburger91@hotmail.com>
2019-07-16 18:58:54 +0200

    refactoring master

---
+++ MonitoringClient/Tests/CustomerValidationTest.cs
@@ -0,0 +1,19 @@
+﻿using MonitoringClient.RegExp;
+using NUnit.Framework;
+
+namespace MonitoringClient.Tests
+{
+    [TestFixture]
+    class CustomerValidationTest
+    {
+        [Test]
+        public void CorrectFirstname_FirstnameValidation_ReturnTrue()
+        {
+            string testFirstname = "Marco";
+            var customerValidation = new CustomerValidation();
+
+            Assert.AreEqual(true, customerValidation.FirstnameValidation(testFirstname));
+
+        }
+    }
+}
