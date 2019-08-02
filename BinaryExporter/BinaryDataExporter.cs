ad3f0438b92d9a8f5685c2e69910dfacd117a9db
M. Waldburger <waldburger91@hotmail.com>
2019-08-02 08:16:04 +0200

    add BinaryDataExporter

---
+++ BinaryExporter/BinaryDataExporter.cs
@@ -0,0 +1,34 @@
+﻿using PluginContracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExporter
+{
+    public class BinaryDataExporter : IDataExportPlugin
+    {
+        public string Name { get; set; }
+
+        public BinaryDataExporter()
+        {
+            Name = "BinaryDataExporter";
+        }
+
+        public void Export(IEnumerable data, string destinationPath)
+        {
+            if(File.Exists(destinationPath))
+            {
+                File.Delete(destinationPath);
+            }
+            FileStream stream = File.Create(destinationPath);
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+    }
+}
