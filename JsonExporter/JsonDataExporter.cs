a1fb6df659c63717ac8feaedf4e84306b179603c
M. Waldburger <waldburger91@hotmail.com>
2019-08-02 12:52:35 +0200

    finish export func

---
+++ JsonExporter/JsonDataExporter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using PluginContracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonExporter
+{
+    public class JsonDataExporter : IDataExportPlugin
+    {
+        public string Name { get; set; }
+
+        public JsonDataExporter()
+        {
+            Name = "JsonDataExporter";
+        }
+
+        public void Export(IEnumerable data, string destinationPath)
+        {
+            if (File.Exists(destinationPath))
+            {
+                File.Delete(destinationPath);
+            }
+            var serializer = new JsonSerializer();
+            var streamWriter = new StreamWriter(destinationPath);
+            var writer = new JsonTextWriter(streamWriter);
+            serializer.Serialize(writer, data);
+        }
+    }
+}
