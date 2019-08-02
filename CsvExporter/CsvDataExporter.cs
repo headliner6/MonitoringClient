95523d4cf531f02d778916bf1c46a2a12426ea88
M. Waldburger <waldburger91@hotmail.com>
2019-08-02 15:49:01 +0200

    add Csvexporter

---
+++ CsvExporter/CsvDataExporter.cs
@@ -0,0 +1,30 @@
+﻿using PluginContracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvExporter
+{
+    public class CsvDataExporter : IDataExportPlugin
+    {
+        public string Name { get; set; }
+
+        public CsvDataExporter()
+        {
+            Name = "CsvDataExporter";
+        }
+
+        public void Export(IEnumerable data, string destinationPath)
+        {
+            if (File.Exists(destinationPath))
+            {
+                File.Delete(destinationPath);
+            }
+            FileStream stream = File.Create(destinationPath);
+        }
+    }
+}
