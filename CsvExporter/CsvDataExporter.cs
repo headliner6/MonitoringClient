0d864f8c0e1e5ea00823a53eb7dc16cf20998d2e
Waldi6 <waldburger91@hotmail.com>
2019-08-13 15:44:23 +0200

    finish Exporter

--- CsvExporter/CsvDataExporter.cs
+++ CsvExporter/CsvDataExporter.cs
@@ -1,6 +1,7 @@
 ﻿using PluginContracts;
 using System;
 using System.Collections;
+using System.Reflection;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,7 +25,36 @@ namespace CsvExporter
             {
                 File.Delete(destinationPath);
             }
-            FileStream stream = File.Create(destinationPath);
+            var csvString = CreateCsvString<IEnumerable>(data);
+            File.WriteAllText(destinationPath, csvString);
+        }
+
+        private string CreateCsvString<T>(IEnumerable data) where T : class
+        {
+            var csvString = "";
+            var delimiter = ";";
+            var properties = data.GetType().GetGenericArguments().FirstOrDefault().GetProperties();
+
+            using (var stringWriter = new StringWriter())
+            {
+                var headerRow = properties
+                .Select(n => n.Name)
+                .Aggregate((columnA, colmnB) => columnA + delimiter + colmnB);
+
+                stringWriter.WriteLine(headerRow);
+
+                foreach (var element in data)
+                {
+                    var positionRow = properties
+                    .Select(n => n.GetValue(element, null))
+                    .Select(n => n == null ? "null" : n.ToString())
+                    .Aggregate((a, b) => a + delimiter + b);
+
+                    stringWriter.WriteLine(positionRow);
+                }
+                csvString = stringWriter.ToString();
+            }
+            return csvString;
         }
     }
 }
