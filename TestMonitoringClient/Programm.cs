ae0699bb77ad1aeb0395297170ac913fe9b07799
Waldi6 <waldburger91@hotmail.com>
2019-08-19 21:02:51 +0200

    change Tests and create TestProject and refactoring

---
+++ TestMonitoringClient/Programm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMonitoringClient
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        { }
+    }
+}
